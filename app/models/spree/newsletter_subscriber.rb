893ac1c937d41663cd4a748bcb84253531fae62a
Michael1969 <sod.michi@gmx.de>
2013-06-20 14:46:01 +1200

    Update newsletter_subscriber.rb
    added first_name, last_name

--- app/models/spree/newsletter_subscriber.rb
+++ app/models/spree/newsletter_subscriber.rb
@@ -3,7 +3,8 @@ module Spree
     validates_presence_of :email
     validates_uniqueness_of :email
     validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-    
-    attr_accessible :email
+    validates_presence_of :first_name
+     validates_presence_of :last_name
+    attr_accessible :email, :first_name, :last_name
   end
 end
