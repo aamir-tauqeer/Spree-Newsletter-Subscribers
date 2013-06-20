f0417edd8a2b1253b377cc81b6eda4aac9a2c9de
Michael1969 <sod.michi@gmx.de>
2013-06-20 17:15:52 +1200

    Update newsletter_subscribers_controller.rb
    changed params to :newsletter_subscribers

--- app/controllers/spree/newsletter_subscribers_controller.rb
+++ app/controllers/spree/newsletter_subscribers_controller.rb
@@ -4,14 +4,14 @@ module Spree
     respond_to :html
 
     def create
-      @newsletter_subscriber = NewsletterSubscriber.new(params[:newsletter_subscriber])
+      @newsletter_subscriber = NewsletterSubscriber.new(params[:newsletter_subscribers])
   
       respond_to do |format|
         if @newsletter_subscriber.save
           flash[:notice] = 'Your email address was successfully added as a subscriber.'
           format.html { redirect_to(root_url) }
         else
-          flash[:error] = 'ERROR- Unable to add your email address as a subscriber.  Perhaps we already have it in our database.'
+          flash[:error] = 'Unable to add your email address as a subscriber. Perhaps we already have it in our database.'
           format.html { redirect_to(root_url) }
         end
       end
