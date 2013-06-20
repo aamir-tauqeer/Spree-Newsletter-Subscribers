3ab36b5018b6533e52a19d5ffa837ca05b3ee12c
Michael1969 <sod.michi@gmx.de>
2013-06-20 17:17:49 +1200

    Update newsletter_subscriber.rb
    changed validators

--- app/models/spree/newsletter_subscriber.rb
+++ app/models/spree/newsletter_subscriber.rb
@@ -1,10 +1,14 @@
 module Spree
   class NewsletterSubscriber < ActiveRecord::Base
-    validates_presence_of :email
-    validates_uniqueness_of :email
-    validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-    validates_presence_of :first_name
-     validates_presence_of :last_name
-    attr_accessible :email, :first_name, :last_name
+   
+   attr_accessible :email, :first_name, :last_name
+    
+    validates :email,
+              :presence => true,
+              :uniqueness => true,
+              :format => {:with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
+    validates :first_name , :presence => true
+    validates :last_name, :presence => true
+  
   end
 end
